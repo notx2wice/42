3fd4179677448dd871d8cb99e1797e4017a22730
ukim <ukim@student.42seoul.kr>
2020-10-03 19:49:11 +0900

    let's go

---
+++ real_42/Libft/ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/03 19:41:09 by ukim              #+#    #+#             */
+/*   Updated: 2020/10/03 19:48:07 by ukim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int i;
+	int minus;
+	int ans;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+	|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+		minus = -1;
+	else
+		minus = 1;
+	if (minus == -1 || str[i] == '+')
+		i++;
+	ans = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		ans = (ans * 10) + (str[i++] - '0');
+	return (ans * minus);
+}
---
+++ real_42/Libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/03 17:30:48 by ukim              #+#    #+#             */
+/*   Updated: 2020/10/03 19:39:57 by ukim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <unistd.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int					ft_atoi(const char *str);
+
+#endif
