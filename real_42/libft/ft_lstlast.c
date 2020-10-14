2b732366a48dce8d76b08b9a3d463dfdf34e45bf
ukim <ukim@student.42seoul.kr>
2020-10-14 14:45:39 +0900

    abort check

--- real_42/libft/ft_lstlast.c
+++ real_42/libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 22:01:03 by ukim              #+#    #+#             */
-/*   Updated: 2020/10/06 16:12:25 by ukim             ###   ########.fr       */
+/*   Updated: 2020/10/14 14:45:19 by ukim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,10 @@
 t_list	*ft_lstlast(t_list *lst)
 {
 	t_list *last;
-
-	while (lst && lst->next)
-	{
+	if (!lst)
+		return (lst);
+	while (lst->next)
 		lst = lst->next;
-	}
 	last = lst;
 	return (last);
 }
