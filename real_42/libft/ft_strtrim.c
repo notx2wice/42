7a98c4f7cfe99ccb5b5b32421014665fe43a5f2c
ukim <ukim@student.42seoul.kr>
2020-10-22 00:56:27 +0900

    strtrim abort

--- real_42/libft/ft_strtrim.c
+++ real_42/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 18:49:46 by ukim              #+#    #+#             */
-/*   Updated: 2020/10/08 20:09:05 by ukim             ###   ########.fr       */
+/*   Updated: 2020/10/22 00:55:26 by ukim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,22 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char		*dest;
 	char		*ans;
 
+	if (!(ans = (char*)malloc(sizeof(char))))
+		return (NULL);
+	ans[0] = '\0';
 	if (!s1 || !set)
 		return (NULL);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	size_s = ft_strlen(s1);
+	if (size_s == 0)
+		return (ans);
 	size_s--;
 	while (size_s && ft_strchr(set, s1[size_s]))
 		size_s--;
 	dest = ft_substr((char*)s1, 0, size_s + 1);
 	if (!dest)
-	{
-		if (!(ans = (char*)malloc(sizeof(char))))
-			return (NULL);
-		ans[0] = '\0';
 		return (ans);
-	}
+	free(ans);
 	return (dest);
 }
