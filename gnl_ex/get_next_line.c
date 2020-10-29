bbae7ff35b2cb73c634e976ce0f88e78be648ac9
ukim <ukim@student.42seoul.kr>
2020-10-30 00:52:53 +0900

    trash

--- gnl_ex/get_next_line.c
+++ gnl_ex/get_next_line.c
@@ -6,37 +6,44 @@
 /*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 15:37:14 by ukim              #+#    #+#             */
-/*   Updated: 2020/10/29 03:26:58 by ukim             ###   ########.fr       */
+/*   Updated: 2020/10/29 21:44:51 by ukim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 static	int		something_in_st(char **st_stored, int fd)// end == 0  fail == -1 normal == 1 잔여물이 남은 체로 들어왔고 잔여물중에 넥라는 없는 것이 확인 된 상태
 {
 	char 	*tmp;
 	char 	*readed;
 	int		ck;
-	int		i;
+	size_t	i;
 
 	i = 0;
-	if (!(readed = (char*)malloc(sizeof(char) * BUFF_SIZE +1)))
+	if (!(readed = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
 	while (i <= BUFF_SIZE)
-		readed[i++] = '\0';
+		readed[i++] = 0;
 	ck = read(fd, readed, BUFF_SIZE);
 	if (ck == -1)
 		return -1;
-	tmp = ft_strjoin(readed, st_stored[fd]);
-	free(st_stored[fd]);
-	free(readed);
+	tmp = ft_strjoin( st_stored[fd],readed);
+	//free(st_stored[fd]);
+	//free(readed);
 	st_stored[fd] = tmp;
-	if (ck == 0)
+	if (ck < BUFF_SIZE)
 		return (0);
-	i = 0;
-	while (i < ft_strlen(tmp))
-		if (tmp[i] == '\n')
-			return (2);
+	i = ft_strlen(tmp) - BUFF_SIZE;
+	if(i > 0)
+	{
+		while (i < ft_strlen(tmp))
+		{
+			if (tmp[i] == '\n')
+				return (2);
+			i++;
+		}
+	}
 	return (1);
 }
 
@@ -48,10 +55,10 @@ static	int		nothing_in_st(char **st_stored, int fd)// end == 0  fail == -1 norma
 
 	ck = 0;
 	i = 0;
-	if (!(tmp = (char*)malloc(sizeof(char) * BUFF_SIZE + 1)))
+	if (!(tmp = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
 	while (i <= BUFF_SIZE)
-		tmp[i++] = '\0';
+		tmp[i++] = 0;
 	ck = read(fd, tmp, BUFF_SIZE);
 	if (ck == -1)
 		return (-1);
@@ -64,40 +71,67 @@ static	int		nothing_in_st(char **st_stored, int fd)// end == 0  fail == -1 norma
 int				get_next_line(int fd, char **line)
 {
 	static char *st_stored[MAX_FILE];
-	char		*tmp_buff;
 	char		*tmp_address;
-	size_t		i;
+	int			i;
 
+	//printf("in fd : %s\n" , st_stored[fd]);
 	i = 0;
-	if (fd < 0 || fd > MAX_FILE || !line || !read(fd, st_stored[fd], 0)
-		|| !(tmp_buff = (char*)malloc(sizeof(char) * BUFF_SIZE + 1)))
+	if (fd < 0 || fd >= MAX_FILE || !line || read(fd, st_stored[fd], 0) == -1)
 		return (-1);
+
 	if (!st_stored[fd])
 	{
 		i = nothing_in_st(st_stored, fd);
 		if (i == 0)
 		{
 			*line = ft_strdup(&st_stored[fd][i]);
-			free(st_stored[fd]);
+			//free(st_stored[fd]);
 			st_stored[fd] = NULL;
 			return (0);
 		}
 		if (i == -1)
 			return (-1);
 	}
-	i = 0;
-	while (st_stored[fd][i] != '\n' || st_stored[fd][i]) // 전에 버퍼에 넥스트라인이 두개들어간 경우 eof인 경우는 이곳에 도달 못함.  read의 반환 값이 다를 경우 끝나는 것으로 하기 때문에
-		i++;
+// 전에 버퍼에 넥스트라인이 두개들어간 경우 eof인 경우는 이곳에 도달 못함.  read의 반환 값이 다를 경우 끝나는 것으로 하기 때문에
+	i = ft_strlen(st_stored[fd]) - BUFF_SIZE;
+	if (i < (int)ft_strlen(st_stored[fd]))
+		while (st_stored[fd][i] != '\n' && st_stored[fd][i])
+			i++;
+	else
+	{
+		i = 0;
+		while (st_stored[fd][i] != '\n' && st_stored[fd][i])
+			i++;
+	}
+
 	if (st_stored[fd][i] == '\n')
 	{
-		*line = ft_substr(st_stored[fd], 0, i - 1);
-		tmp_address = ft_strdup(&st_stored[fd][i]);
-		free(st_stored[fd]);
+		tmp_address = 0;
+		*line = ft_substr(st_stored[fd], 0, i);
+		if (i + 1 < BUFF_SIZE)
+			tmp_address = ft_strdup(&st_stored[fd][i+1]);
+		//free(st_stored[fd]);
 		st_stored[fd] = tmp_address;
 		return (1);
 	}
 	i = 1;
+	int j = 0;
 	while (i == 1)
+	{
 		i = something_in_st(st_stored, fd);
-	return (1);
+	//	printf("in while fd : %s\n" , st_stored[fd]);
+	}
+	if (i == -1)
+		return (-1);
+	if (i == 2)
+	{
+		while (st_stored[fd][j] != '\n')
+			j++;
+		*line = ft_substr(st_stored[fd], 0, j);
+		st_stored[fd] = &st_stored[fd][j+1];
+		return (1);
+	}
+	if (i == 0)
+		*line = ft_substr(st_stored[fd], 0, ft_strlen(st_stored[fd]));
+	return (0);
 }
---
+++ gnl_ex/main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/09 16:05:34 by jaleman           #+#    #+#             */
+/*   Updated: 2020/10/29 18:29:11 by ukim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include "get_next_line.h"
+
+int main(int argc, char **argv)
+{
+	int fd;
+	int ret;
+	int line;
+	char *buff;
+// buff  한줄읽은 값이 들어올 공간.
+	line = 0;
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		while ((ret = get_next_line(fd, &buff)) > 0)
+		{
+			printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+			free(buff);
+		}
+		printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+		if (ret == -1)
+			printf("-----------\nError\n");
+		else if (ret == 0)
+			printf("-----------\nEnd of file\n");
+		close(fd);
+	}
+	if (argc == 1)
+	{
+		while ((ret = get_next_line(0, &buff)) > 0)
+			printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+		if (ret == -1)
+			printf("-----------\nError\n");
+		else if (ret == 0)
+			printf("-----------\nEnd of stdin\n");
+		close(fd);
+	}
+	return (0);
+}
