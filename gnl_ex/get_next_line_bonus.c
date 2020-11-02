2774497f7c4b416ef58bd477cd1f1b7dc9eb9d11
Ukhwi Kim <ukim@c5r3s5.42seoul.kr>
2020-11-02 19:14:14 +0900

    done

---
+++ gnl_ex/get_next_line_bonus.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 15:37:14 by ukim              #+#    #+#             */
+/*   Updated: 2020/11/01 22:17:06 by ukim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static	int		something_in_st(char **st_stored, int fd)
+{
+	char		*tmp;
+	char		*readed;
+	int			ck;
+	int			i;
+
+	i = 0;
+	if (!(readed = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while (i <= BUFFER_SIZE)
+		readed[i++] = 0;
+	ck = read(fd, readed, BUFFER_SIZE);
+	if (ck == -1)
+		return (-1);
+	tmp = ft_strjoin(st_stored[fd], readed);
+	free(st_stored[fd]);
+	free(readed);
+	st_stored[fd] = tmp;
+	if (ck < BUFFER_SIZE)
+		return (0);
+	i = (int)ft_strlen(tmp) - BUFFER_SIZE;
+	if (i > 0)
+		while (i < (int)ft_strlen(tmp))
+			if (tmp[i++] == '\n')
+				return (2);
+	return (1);
+}
+
+static	int		nothing_in_st(char **st_stored, int fd)
+{
+	char		*tmp;
+	int			i;
+	int			ck;
+
+	ck = 0;
+	i = 0;
+	if (!(tmp = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while (i <= BUFFER_SIZE)
+		tmp[i++] = 0;
+	ck = read(fd, tmp, BUFFER_SIZE);
+	if (ck == -1)
+		return (-1);
+	st_stored[fd] = tmp;
+	if (ck != BUFFER_SIZE)
+		return (0);
+	return (1);
+}
+
+static	int		fill_something(int fd, char **st_stored)
+{
+	int			i;
+
+	i = nothing_in_st(st_stored, fd);
+	if (i == -1)
+		free(st_stored[fd]);
+	return (i);
+}
+
+static	int		after_work(int fd, char **line, char **st_stored, int i)
+{
+	char		*tmp_address;
+
+	if (i == -1)
+		return (-1);
+	if (i == 2)
+	{
+		i = 0;
+		while (st_stored[fd][i] != '\n')
+			i++;
+		*line = ft_substr(st_stored[fd], 0, i);
+		tmp_address = ft_strdup(&st_stored[fd][i + 1]);
+		free(st_stored[fd]);
+		st_stored[fd] = tmp_address;
+		return (1);
+	}
+	if (i == 0)
+	{
+		i = check_b_n(st_stored, fd, line);
+		if (i == 1)
+			return (1);
+		*line = ft_substr(st_stored[fd], 0, ft_strlen(st_stored[fd]));
+		free(st_stored[fd]);
+		st_stored[fd] = 0;
+	}
+	return (0);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	static char	*st_stored[MAX_FILE + 1];
+	int			i;
+
+	i = BUFFER_SIZE;
+	if (fd < 0 || fd >= MAX_FILE || !line
+	|| read(fd, st_stored[fd], 0) == -1 || i <= 0)
+		return (-1);
+	if (!st_stored[fd])
+	{
+		i = fill_something(fd, st_stored);
+		if (i == -1)
+			return (i);
+	}
+	i = check_b_n(st_stored, fd, line);
+	if (i)
+		return (i);
+	i = 1;
+	while (i == 1)
+		i = something_in_st(st_stored, fd);
+	return (after_work(fd, line, st_stored, i));
+}
---
+++ gnl_ex/get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 15:37:18 by ukim              #+#    #+#             */
+/*   Updated: 2020/11/01 22:17:04 by ukim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define MAX_FILE 1025
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		check_b_n(char **st_stored, int fd, char **line);
+int		get_next_line(int const fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
