2daa8008a3f6f3561dcda575715129dc9f110a73
Ukhwi Kim <ukim@c5r6s3.42seoul.kr>
2020-11-12 19:45:14 +0900

    compilable status

--- printf/libft/ft_atoi.c
+++ printf/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ukim <ukim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 19:41:09 by ukim              #+#    #+#             */
-/*   Updated: 2020/11/12 17:54:27 by ukim             ###   ########.fr       */
+/*   Updated: 2020/11/12 19:43:47 by ukim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ int			ft_atoi(char **str)
 	num = 0;
 	sign = 1;
 	
-	if (*str == '-' || *str == '+')
+	if (**str == '-' || **str == '+')
 	{
-		if (*str == '-')
+		if (**str == '-')
 			sign *= -1;
-		str++;
+		(*str)++;
 	}
-	while (*str && ft_isdigit(*str))
+	while (**str && ft_isdigit(**str))
 	{
 		num *= 10;
-		num += *str - '0';
-		str++;
+		num += **str - '0';
+		(*str)++;
 	}
 	return (sign * num);
 }
